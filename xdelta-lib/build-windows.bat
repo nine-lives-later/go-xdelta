a4746ec86b2c77973b2a7a36c75ab1fe02bb91ca
Felix Kollmann <fk@konsorten.de>
2019-02-28 00:02:21 +0100

    Merge some changes from cirrus-test branch

--- xdelta-lib/build-windows.bat
+++ xdelta-lib/build-windows.bat
@@ -1,8 +1,16 @@
 @echo off
 
+pushd "%~dp0"
+
 setlocal
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+)
+
+if exist "C:\BuildTools\VC" (
+    call "C:\BuildTools\VC\vcvarsall.bat" amd64
+)
 
 cl.exe /nologo /I src /MT /LD /GL /Fe:go-xdelta-lib.dll xdelta.cpp xdelta-encoder.cpp xdelta-decoder.cpp xdelta-go-helpers.cpp xdelta-go-encoder.cpp xdelta-go-decoder.cpp /link /RELEASE /LTCG /NOLOGO /VERSION:3.1
 
@@ -13,3 +21,11 @@ REM dumpbin.exe /nologo /exports go-xdelta-lib.dll
 powershell.exe -ExecutionPolicy Unrestricted -File build-windows-write-version.ps1
 
 endlocal
+
+popd
+
+if not exist "%~dp0go-xdelta-lib.dll" (
+    exit /b 1
+)
+
+exit /b 0
